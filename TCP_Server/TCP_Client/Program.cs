2011f09858464a71b275eb5bbb909c4f2df1846a
SweetFairyTale <792119047@qq.com>
2019-03-09 22:02:11 +0800

    client-server architecture
    C# -- Socket

---
+++ TCP_Server/TCP_Client/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace TCP_Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            clientSocket.Connect(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 2049));
+
+            byte[] data = new byte[1024];
+            int count = clientSocket.Receive(data);
+            string msg = Encoding.UTF8.GetString(data, 0, count);
+            Console.WriteLine(msg);
+            
+            while(true)  //test
+            {
+                string str = Console.ReadLine();
+                if(str == "c")
+                {
+                    clientSocket.Close();
+                    return;
+                }
+                clientSocket.Send(Encoding.UTF8.GetBytes(str));
+            }
+
+            Console.ReadKey();
+            clientSocket.Close();
+
+        }
+    }
+}
---
+++ TCP_Server/TCP_Server/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Text;
+using System.Net.Sockets;
+using System.Net;
+
+namespace TCP_Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StartServerAsync();
+            Console.ReadKey();
+        }
+
+        static byte[] dataBuffer = new byte[1024];
+
+        static void StartServerAsync()
+        {
+            Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPAddress ipAddress = IPAddress.Parse("127.0.0.1");  //47.100.2.223
+            IPEndPoint ipEndPoint = new IPEndPoint(ipAddress, 2049);
+            serverSocket.Bind(ipEndPoint);  //绑定ip与端口号.
+            serverSocket.Listen(10);  //同时等待处理的队列长度(不是最大玩家数).
+
+            //Socket clientSocket = serverSocket.Accept();  //接收一个客户端连接.(改异步)
+
+            serverSocket.BeginAccept(AcceptCallBack, serverSocket);
+
+        }
+
+        static void AcceptCallBack(IAsyncResult ar)
+        {
+            Socket serverSocket = ar.AsyncState as Socket;
+            Socket clientSocket = serverSocket.EndAccept(ar);
+
+            //向客户端发送一条消息.
+            string msg = "hello,你好。";
+            byte[] data = Encoding.UTF8.GetBytes(msg);
+            clientSocket.Send(data);
+
+            //接收客户端的多次消息发送.            
+            clientSocket.BeginReceive(dataBuffer, 0, 1024, SocketFlags.None, ReceiveCallBack, clientSocket);
+            //等待下一个客户端的连接.
+            serverSocket.BeginAccept(AcceptCallBack, serverSocket);
+        }
+
+        static void ReceiveCallBack(IAsyncResult ar)
+        {
+            Socket clientSocket = null;
+            try
+            {
+                clientSocket = ar.AsyncState as Socket;
+                int count = clientSocket.EndReceive(ar);
+                if(count == 0)
+                {
+                    clientSocket.Close();
+                    return;
+                }
+                string msg = Encoding.UTF8.GetString(dataBuffer, 0, count);
+                Console.WriteLine("异步接受的数据：" + msg);
+                clientSocket.BeginReceive(dataBuffer, 0, 1024, SocketFlags.None, ReceiveCallBack, clientSocket);  //循环回调.
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e);
+                if(clientSocket != null)
+                {
+                    clientSocket.Close();
+                }
+            }    
+            
+        }
+
+        static void StartServerSync()
+        {
+            Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPAddress ipAddress = IPAddress.Parse("127.0.0.1");  //47.100.2.223
+            IPEndPoint ipEndPoint = new IPEndPoint(ipAddress, 2049);
+            serverSocket.Bind(ipEndPoint);  //绑定ip与端口号.
+            serverSocket.Listen(10);  //同时等待处理的队列长度(不是最大玩家数).
+            Socket clientSocket = serverSocket.Accept();  //接收一个客户端连接.
+
+            //向客户端发送一条消息.
+            string msg = "hello, 你好。";
+            byte[] data = Encoding.UTF8.GetBytes(msg);
+            clientSocket.Send(data);
+
+            //接收客户端的一条消息.
+            //byte[] dataBuffer = new byte[1024];
+            int count = clientSocket.Receive(dataBuffer);
+            string msgReceive = Encoding.UTF8.GetString(dataBuffer, 0, count);
+
+            Console.WriteLine(msgReceive);
+
+            clientSocket.Close();
+            serverSocket.Close();
+        }
+    }
+}
