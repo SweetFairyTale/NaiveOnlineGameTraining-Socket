1999949c7d22c4c66481f0d63e3b777d0f2dd754
SweetFairyTale <792119047@qq.com>
2019-03-17 22:23:40 +0800

    Byte Conversion

---
+++ TCP_Server/ConvertToByteArray/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+
+
+namespace ConvertToByteArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int temp = 100;
+            byte[] data = BitConverter.GetBytes(temp);
+            foreach(byte b in data)
+            {
+                Console.Write(b + ":");
+            }
+            Console.ReadKey();
+        }
+    }
+}
