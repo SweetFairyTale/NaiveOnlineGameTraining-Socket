6c813425e8370e174008b16894511c0960a35296
SweetFairyTale <792119047@qq.com>
2019-10-22 16:01:22 +0800

    ForestWar客户端
    game client

---
+++ Forest War/Assets/Scripts/GameFacade.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameFacade : MonoBehaviour {
+
+    private UIManager uiManager;
+    private AudioManager audioManager;
+    private CameraManager cameraManager;
+    private PlayerManager playerManager;
+    private RequestManager requestManager;
+    private ClientManager clientManager;
+
+
+	// Use this for initialization
+	void Start () {
+        InitManager();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void InitManager()
+    {
+        uiManager = new UIManager();
+        audioManager = new AudioManager();
+        cameraManager = new CameraManager();
+        playerManager = new PlayerManager();
+        requestManager = new RequestManager();
+        clientManager = new ClientManager();
+
+        uiManager.OnInit();
+        audioManager.OnInit();
+        cameraManager.OnInit();
+        playerManager.OnInit();
+        requestManager.OnInit();
+        clientManager.OnInit();
+    }
+
+    private void DestoryManager()
+    {
+        uiManager.OnDestroy();
+        audioManager.OnDestroy();
+        cameraManager.OnDestroy();
+        playerManager.OnDestroy();
+        requestManager.OnDestroy();
+        clientManager.OnDestroy();
+    }
+
+    private void OnDestroy()
+    {
+        DestoryManager();
+    }
+}
---
+++ Forest War/Assets/Scripts/Manager/BaseManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseManager{
+
+    public virtual void OnInit()
+    {
+
+    }
+
+    public virtual void OnDestroy()
+    {
+
+    }
+}
